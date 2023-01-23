fc54124b85a0d743c3749c95b714b2d26a100dd8
Marcin <marcin92d@gmail.com>
2023-01-23 18:06:34 +0100

    Calculator implemented

--- src/main/java/pl/dolega/App.java
+++ src/main/java/pl/dolega/App.java
@@ -6,8 +6,14 @@ package pl.dolega;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        Calculator calculator = new Calculator();
+
+        calculator.addition(3, 2);
+        calculator.substraction(3, 2);
+        calculator.multiplication(3, 2);
+        System.out.println(calculator.division(3, 2));
+
     }
 }
---
+++ src/main/java/pl/dolega/Calculator.java
@@ -0,0 +1,20 @@
+package pl.dolega;
+
+public class Calculator {
+
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    public int substraction(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a  * b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
+    }
+}
